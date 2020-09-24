8b56e1ba8528d8a3af3f74eddd2e1d15c463258c
lupyuen <luppy@appkaki.com>
2020-09-24 17:57:29 +0800

    Link with LVGL OK

---
+++ src/pinetime/lvgl.c
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+//  LVGL Interface for PineTime on Mynewt
+
+#include <os/os.h>
+#include <hal/hal_bsp.h>
+#include <hal/hal_gpio.h>
+#include <hal/hal_system.h>
+#include <hal/hal_flash.h>
+#include <console/console.h>
+
+/// Init the LVGL library. Called by sysinit() during startup, defined in pkg.yml.
+void pinetime_lvgl_mynewt_init(void) {    
+    console_printf("Init LVGL..."); console_flush();
+}
